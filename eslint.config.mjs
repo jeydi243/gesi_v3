856f13cf9cb88f1c0990029b45cab47005efa7ea
KADIONGO ILUNGA Epaphrodite <kadiongo.ilunga@gecamines.cd>
2024-05-26 19:41:03 +0200

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/first-attribute-linebreak': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-closing-bracket-newline': 'off'
+  }
+})
